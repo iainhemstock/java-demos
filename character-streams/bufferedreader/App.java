e45953eb1117547724700b9ceb15bcd3f7523341
Unknown <iainhemstock@googlemail.com>
2019-11-23 21:00:38 +0000

    add demo (BufferedReader)

---
+++ character-streams/bufferedreader/App.java
@@ -0,0 +1,89 @@
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.Reader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.io.BufferedWriter;
+import java.lang.Character;
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class App {
+    public static void main(String[] args) {
+        new App();
+    }
+
+    public App () {
+        try (BufferedReader br = new BufferedReader(new FileReader("lyrics.txt"))) {
+            br.mark(1000);
+            readCharAtATime(br);
+            br.reset();
+            readLineAtATime(br);
+            br.reset();
+            readLinesByStreamIntoList(br);
+            br.reset();
+            readLinesByStreamIntoStringBuilder(br);
+            br.reset();
+            writeUppercaseLyricsToFile(br, "uppercase_lyrics.txt");
+        }
+        catch(FileNotFoundException ex) { ex.printStackTrace(); }
+        catch(IOException ex) { ex.printStackTrace(); }
+    }
+
+    private void readCharAtATime(Reader reader) {
+        System.out.println("Reading a character at a time:");
+        try {
+            int c;
+            while((c = reader.read()) != -1) {
+                System.out.print((char)c);
+            }
+            System.out.println("");
+        }
+        catch(IOException ex) { ex.printStackTrace(); }
+    }
+
+    private void readLineAtATime(BufferedReader reader) {
+        System.out.println("Reading a line at a time:");
+        String line = "";
+        try {
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            System.out.println("");
+        }
+        catch(IOException ex) { ex.printStackTrace(); }
+    }
+
+    private void readLinesByStreamIntoList(BufferedReader reader) throws IOException {
+        System.out.println("Reading lines from stream into a list:");
+
+        List<String> lines = reader.lines().collect(Collectors.toList());
+        System.out.println(lines);
+        System.out.println("");
+    }
+
+    private void readLinesByStreamIntoStringBuilder(BufferedReader reader) throws IOException {
+        System.out.println("Reading lines from stream into a stringbuilder:");
+
+        StringBuilder sb = new StringBuilder();
+        reader.lines().forEach(line -> sb.append(line + System.lineSeparator()));
+        System.out.println(sb.toString());
+        System.out.println("");
+    }
+
+    private void writeUppercaseLyricsToFile(BufferedReader br, String filename) {
+        // could use the form:
+        // BufferedWriter bw = new BufferedWriter(new FileWriter(filename))
+        // to create a BufferedWriter
+        try (BufferedWriter bw = Files.newBufferedWriter(Paths.get(filename))) {
+            int c;
+            while ((c = br.read()) != -1) {
+                bw.write(Character.toUpperCase((char)c));
+            }
+        }
+        catch(Exception ex) { ex.printStackTrace(); }
+    }
+}
